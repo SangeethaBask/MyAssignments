d8661453efc98780016d35f4696d75fd130cfcc4
SangeethaBask <153972590+SangeethaBask@users.noreply.github.com>
2023-12-16 10:47:30 +0530

    Add files via upload
    Week1 - Day1

---
+++ week1/LoginPage.java
@@ -0,0 +1,10 @@
+package week1;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		ChromeDriver driver =new ChromeDriver();
+	}
+			
+}
---
+++ week1/LoginPage_New.java
@@ -0,0 +1,12 @@
+package week1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage_New {
+	public static void main(String[] args) {
+		ChromeDriver driver=new  ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+	}
+
+}
